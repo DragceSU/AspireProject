2fed4508c4f93a15698fc154d154e3c6b40b889e
Dragan Shutinovski <Dragan.Shutinovski@endava.com>
2025-12-15 11:36:20 +0100

    Update Aspire and add the microservices

--- AppHost/AppHost/AppHost.cs
+++ AppHost/AppHost/AppHost.cs
@@ -3,5 +3,7 @@ using Projects;
 var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<WebApi_Service>("webapi-service");
+builder.AddProject<InvoiceMicroservice>("invoice-microservice");
+builder.AddProject<PaymentMicroservice>("payment-microservice");
 
 builder.Build().Run();
